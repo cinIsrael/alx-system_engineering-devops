067d2f157774fa8a8d4d5ae00838f1f7225791c6
cinisrael <Israelabina05@gmail.com>
2024-08-27 06:11:06 +0000

    update

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+vagrant@ubuntu:~$ sudo puppet apply 100-puppet_ssh_config.pp
+Notice: Compiled catalog for ubuntu-xenial in environment production in 0.11 seconds
+Notice: /Stage[main]/Main/File_line[Turn off passwd auth]/ensure: created
+Notice: /Stage[main]/Main/File_line[Declare identity file]/ensure: created
+Notice: Finished catalog run in 0.03 seconds
+vagrant@ubuntu:~$
